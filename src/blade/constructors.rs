757ea8e26aa144ffc6907e6f45e9f52feffae69b
Joshua Smith <jsmith62831@gmail.com>
2021-07-18 21:02:04 -0500

    added docs to Blade::new()

--- src/blade/constructors.rs
+++ src/blade/constructors.rs
@@ -1,52 +1,6 @@
 
 use super::*;
 
-macro_rules! impl_static_constructors {
-    ($($ty:ident::new($($arg:ident),*);)*) => {
-        $(
-            impl<T> $ty<T> {
-                pub fn new($($arg: T),*) -> $ty<T> {
-                    $ty { data: [ $($arg),* ] }
-                }
-            }
-        )*
-    }
-}
-
-impl_static_constructors!{
-
-    Vec1::new(x);
-    Vec2::new(x,y);
-    Vec3::new(x,y,z);
-    Vec4::new(x,y,z,w);
-    Vec5::new(x,y,z,w,a);
-    Vec6::new(x,y,z,w,a,b);
-
-    BiVec2::new(x);
-    BiVec3::new(x,y,z);
-    BiVec4::new(yz,zx,xy,xw,yw,zw);
-    BiVec5::new(yz,zx,xy,xw,yw,zw,xa,ya,za,wa);
-    BiVec6::new(yz,zx,xy,xw,yw,zw,xa,ya,za,wa,xb,yb,zb,wb,ab);
-
-    TriVec3::new(x);
-    TriVec4::new(x,y,z,w);
-    TriVec5::new(yz,zx,xy,xw,yw,zw,xa,ya,za,wa);
-    TriVec6::new(
-        e145, e245, e345, e235, e315, e125, e324, e134, e214, e123,
-        e326, e136, e216, e416, e426, e436, e516, e526, e536, e546
-    );
-
-    QuadVec4::new(x);
-    QuadVec5::new(x,y,z,w,a);
-    QuadVec6::new(yz,zx,xy,xw,yw,zw,xa,ya,za,wa,xb,yb,zb,wb,ab);
-
-    PentVec5::new(x);
-    PentVec6::new(x,y,z,w,a,b);
-
-    HexVec6::new(x);
-
-}
-
 impl<T:Alloc<N,G>, N:Dim, G:Dim> Blade<T,N,G> {
 
     pub fn from_iter_generic<I:IntoIterator<Item=T>>(n:N, g:G, iter: I) -> Self {
@@ -144,5 +98,54 @@ impl<T:Alloc<Dynamic,Dynamic>> Blade<T,Dynamic,Dynamic> {
     pub fn zeroed(n:usize, g:usize) -> Self where T:Zero {
         Self::zeroed_generic(Dynamic::new(n), Dynamic::new(g))
     }
+
+}
+
+macro_rules! impl_static_constructors {
+    ($($ty:ident::new($($arg:ident),*);)*) => {
+        $(
+            impl<T> $ty<T> {
+                #[doc = concat!(
+                    "Constructs a [`", stringify!($ty), "`] directly from components"
+                )]
+                pub fn new($($arg: T),*) -> $ty<T> {
+                    $ty { data: [ $($arg),* ] }
+                }
+            }
+        )*
+    }
+}
+
+impl_static_constructors!{
+
+    Vec1::new(x);
+    Vec2::new(x,y);
+    Vec3::new(x,y,z);
+    Vec4::new(x,y,z,w);
+    Vec5::new(x,y,z,w,a);
+    Vec6::new(x,y,z,w,a,b);
+
+    BiVec2::new(x);
+    BiVec3::new(x,y,z);
+    BiVec4::new(yz,zx,xy,xw,yw,zw);
+    BiVec5::new(yz,zx,xy,xw,yw,zw,xa,ya,za,wa);
+    BiVec6::new(yz,zx,xy,xw,yw,zw,xa,ya,za,wa,xb,yb,zb,wb,ab);
+
+    TriVec3::new(x);
+    TriVec4::new(x,y,z,w);
+    TriVec5::new(yz,zx,xy,xw,yw,zw,xa,ya,za,wa);
+    TriVec6::new(
+        e145, e245, e345, e235, e315, e125, e324, e134, e214, e123,
+        e326, e136, e216, e416, e426, e436, e516, e526, e536, e546
+    );
+
+    QuadVec4::new(x);
+    QuadVec5::new(x,y,z,w,a);
+    QuadVec6::new(yz,zx,xy,xw,yw,zw,xa,ya,za,wa,xb,yb,zb,wb,ab);
+
+    PentVec5::new(x);
+    PentVec6::new(x,y,z,w,a,b);
+
+    HexVec6::new(x);
 
 }
