1a4468e1c2fcde855a0d781cefc4dcdb8a72c89f
Joshua Smith <jsmith62831@gmail.com>
2021-12-11 20:05:56 -0600

    doc comments for crate::base::coordinates

--- src/base/coordinates.rs
+++ src/base/coordinates.rs
@@ -1,4 +1,10 @@
-
+//!
+//! Structs to dereference the algebraic types into to access their elements as if they
+//! were fields.
+//!
+//! This module mostly just wraps [`na::base::coordinates`], but a couple extra structs were added
+//! in order to support some of the structures unique to this crate
+//!
 
 use std::ops::{Deref, DerefMut};
 
@@ -7,12 +13,14 @@ use na::Scalar;
 use crate::algebra::*;
 pub use na::base::coordinates::*;
 
+/// Fields for scalar and psuedoscalar types
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct ScalarCoords<T:Scalar> {
     pub value: T,
 }
 
+/// Fields specifically for 4D bivectors
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct BiVec4Coords<T:Scalar> {
@@ -24,6 +32,7 @@ pub struct BiVec4Coords<T:Scalar> {
     pub zw: T,
 }
 
+/// Fields for 2D [`Even`]s that emulate the components of complex numbers
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct ReIm<T:Scalar> {
@@ -31,6 +40,12 @@ pub struct ReIm<T:Scalar> {
     pub im:T,
 }
 
+///
+/// Fields for 3D [`Even`]s that emulate the components of quaternions
+///
+/// The order here is different from `nalgebra`'s [quaternions](na::geometry::Quaternion).
+/// (`nalgebra` uses [ijkw](IJKW) order)
+///
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct WIJK<T:Scalar> {
