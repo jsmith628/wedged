3c235fc810314432bb9b4756847adcfd258345da
Joshua Smith <jsmith62831@gmail.com>
2021-12-11 20:43:21 -0600

    expanded binom LUT

--- src/base/count.rs
+++ src/base/count.rs
@@ -42,18 +42,28 @@
 pub const fn binom(n:usize, k:usize) -> usize {
 
     //LUTs for the win!
-    const LUT: &[&[usize]] = &[
-        &[1, 0,  0,  0,  0, 0, 0],
-        &[1, 1,  0,  0,  0, 0, 0],
-        &[1, 2,  1,  0,  0, 0, 0],
-        &[1, 3,  3,  1,  0, 0, 0],
-        &[1, 4,  6,  4,  1, 0, 0],
-        &[1, 5, 10, 10,  5, 1, 0],
-        &[1, 6, 15, 20, 15, 6, 1],
+    //yes, yes, I should probably do this programmatically, but it was fun and I wanted to type it all out
+    const LUT: [[usize; 16]; 16] = [
+        [1, 0,    0,   0,   0,     0,    0,    0,    0,    0,    0,    0,   0,   0,  0, 0],
+        [1, 1,    0,   0,   0,     0,    0,    0,    0,    0,    0,    0,   0,   0,  0, 0],
+        [1, 2,    1,   0,   0,     0,    0,    0,    0,    0,    0,    0,   0,   0,  0, 0],
+        [1, 3,    3,   1,   0,     0,    0,    0,    0,    0,    0,    0,   0,   0,  0, 0],
+        [1, 4,    6,   4,   1,     0,    0,    0,    0,    0,    0,    0,   0,   0,  0, 0],
+        [1, 5,   10,  10,   5,     1,    0,    0,    0,    0,    0,    0,   0,   0,  0, 0],
+        [1, 6,   15,  20,  15,     6,    1,    0,    0,    0,    0,    0,   0,   0,  0, 0],
+        [1, 7,   21,  35,  35,    21,    7,    1,    0,    0,    0,    0,   0,   0,  0, 0],
+        [1, 8,   28,  56,  70,    56,   28,    8,    1,    0,    0,    0,   0,   0,  0, 0],
+        [1, 9,   36,  84,  126,  126,   84,   36,    9,    1,    0,    0,   0,   0,  0, 0],
+        [1, 10,  45, 120,  210,  252,  210,  120,   45,   10,    1,    0,   0,   0,  0, 0],
+        [1, 11,  55, 165,  330,  462,  462,  330,  165,   55,   11,    1,   0,   0,  0, 0],
+        [1, 12,  66, 220,  495,  792,  924,  792,  495,  220,   66,   12,   1,   0,  0, 0],
+        [1, 13,  78, 286,  715, 1287, 1716, 1716, 1287,  715,  286,   78,  13,   1,  0, 0],
+        [1, 14,  91, 364, 1001, 2002, 3003, 3432, 3003, 2002, 1001,  364,  91,  14,  1, 0],
+        [1, 15, 105, 455, 1365, 3003, 5005, 6435, 6435, 5005, 3003, 1365, 455, 105, 15, 1],
     ];
 
     //should make some stuff run a little faster
-    if n<=6 && k<=6 { return LUT[n][k]; }
+    if n<16 && k<16 { return LUT[n][k]; }
 
     //base cases
     if k>n { return 0; }
@@ -163,4 +173,27 @@ pub fn even_components_of(n: usize) -> impl std::iter::Iterator<Item=usize> {
 ///
 pub fn odd_components_of(n: usize) -> impl std::iter::Iterator<Item=usize> {
     components_of(n).skip(1).step_by(2)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    const fn direct_binom(n: usize, k: usize) -> usize {
+        if k>n { return 0; }
+        if k==0 { return 1; }
+        (n-k+1) * direct_binom(n, k-1) / k
+    }
+
+    //wanna make sure I did the math right on the LUT
+    #[test]
+    fn test_lut() {
+        for n in 0..=16 {
+            for k in 0..=16 {
+                assert_eq!(binom(n,k), direct_binom(n,k));
+            }
+        }
+    }
+
 }
